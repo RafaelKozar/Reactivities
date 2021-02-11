f750ef4f2559ccea082fc623083a57c7f6263cd1
RafaelKozar <rafael_kozar@hotmail.com>
2021-02-11 16:03:26 -0300

    end of section 9 - reestyling

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,18 +1,20 @@
 import { observer } from "mobx-react-lite";
 import React, { useContext, useEffect } from "react";
 import { RouteComponentProps } from "react-router";
-import { Link } from "react-router-dom";
-import { Button, Card, Image } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import { LoadingComponent } from "../../../app/layout/LoadingComponent";
 import ActivityStore from "../../../app/stores/activityStore";
+import  ActivityDetailedChat  from "./ActivityDetailedChat";
+import  ActivityDetailedHeader  from "./ActivityDetailedHeader";
+import  ActivityDetailedInfo from "./ActivityDetailedInfo";
+import  ActivityDetailedSidebar from "./ActivityDetailedSidebar";
 
 interface DetailsParams {
   id: string;
 }
 
 const ActivityDetails: React.FC<RouteComponentProps<DetailsParams>> = ({
-  match, history
-}) => {
+  match}) => {
   const activityStore = useContext(ActivityStore);
   const {
     activity,    
@@ -28,36 +30,17 @@ const ActivityDetails: React.FC<RouteComponentProps<DetailsParams>> = ({
 
   return (
     <div>
-      <Card>
-        <Image
-          src={`/assets/categoryImages/${activity!.category}.jpg`}
-          wrapped
-          ui={false}
-        />
-        <Card.Content>
-          <Card.Header>{activity!.title}</Card.Header>
-          <Card.Meta>
-            <span>{activity!.date}</span>
-          </Card.Meta>
-          <Card.Description>{activity!.description}</Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <Button.Group widths={2}>
-            <Button
-              basic
-              color="blue"
-              as={Link} to={`/manage/${activity.id}`}
-              content="Edit"
-            />
-            <Button
-              basic
-              color="grey"
-              onClick={() => history.push('/activities')}
-              content="Cancel"
-            />
-          </Button.Group>
-        </Card.Content>
-      </Card>
+      <Grid>
+        <Grid.Column width={10}>
+          <ActivityDetailedHeader activity={activity}/>
+          <ActivityDetailedInfo  activity={activity}/>
+          <ActivityDetailedChat />
+        </Grid.Column>
+        <Grid.Column width={6}>
+          <ActivityDetailedSidebar />
+        </Grid.Column>
+      </Grid>
+
     </div>
   );
 };
--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,14 +1,50 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Container } from 'semantic-ui-react'
+
+  
+import React, { useContext, Fragment } from 'react';
+import { Container, Segment, Header, Button, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+// import { RootStoreContext } from '../../app/stores/rootStore';
+// import LoginForm from '../user/LoginForm';
+// import RegisterForm from '../user/RegisterForm';
 
 const HomePage = () => {
-    return (
-        <Container style={{marginTop : '7em'}}>
-            <h1>Home Page</h1>
-            <h3>Go to <Link to='/activities'>Activities</Link></h3>
-        </Container>
-    );
+  const token = window.localStorage.getItem('jwt');
+//   const rootStore = useContext(RootStoreContext);
+//   const { user, isLoggedIn } = rootStore.userStore;
+//   const {openModal} = rootStore.modalStore;
+  return (
+    <Segment inverted textAlign='center' vertical className='masthead'>
+      <Container text>
+        <Header as='h1' inverted>
+          <Image
+            size='massive'
+            src='/assets/logo.png'
+            alt='logo'
+            style={{ marginBottom: 12 }}
+          />
+          Reactivities
+        </Header>
+        {/* {isLoggedIn && user && token ? ( */}
+          <Fragment>
+            {/* <Header as='h2' inverted content={`Welcome back ${user.displayName}`} /> */}
+            <Button as={Link} to='/activities' size='huge' inverted>
+              Go to activities!
+            </Button>
+          </Fragment>
+        {/* ) : ( */}
+          <Fragment>
+          <Header as='h2' inverted content={`Welcome to Reactivitities`} />
+          <Button onClick={() => {}} size='huge' inverted>
+            Login
+          </Button>
+          <Button onClick={() => {}} size='huge' inverted>
+            Register
+          </Button>
+        </Fragment>
+        {/* )} */}
+      </Container>
+    </Segment>
+  );
 };
 
 export default HomePage;
