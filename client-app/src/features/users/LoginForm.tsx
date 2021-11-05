1e1611f6d8e859bd96c921e4f65b1f5b7df55be8
Rafael Kozar <rafael_kozar@hotmail.com>
2021-11-05 16:44:03 -0300

    Login response error

--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -1,26 +1,32 @@
-import { Form, Formik } from 'formik'
+import { ErrorMessage, Form, Formik } from 'formik'
 import { observer } from 'mobx-react-lite'
 import React, { useState } from 'react'
-import { Button } from 'semantic-ui-react'
+import { Button, Label } from 'semantic-ui-react'
 import MyTextInput from '../../app/common/form/MyTextInput'
 import { useStore } from '../../app/stores/store'
 import { UserFormValues } from "../../app/models/user";
 
+
 export default observer(function LoginForm() {
-    const  {userStore} = useStore();
+    const { userStore } = useStore();
 
     return (
         <Formik
-            initialValues={{email : '', password : ''}}
-            onSubmit={(values : UserFormValues) => userStore.login(values)}>
+            initialValues={{ email: '', password: '', error: null }}
+            onSubmit={(values, { setErrors }) => userStore.login(values).catch(error =>
+                setErrors({ error: 'Invalid email or password' }))}>
 
-                {({handleSubmit, isSubmitting}) => (
-                    <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
-                        <MyTextInput name='email' placeholder='Email' />
-                        <MyTextInput name='password' placeholder='Password' type='password' />
-                        <Button loading={isSubmitting} positive content='Login' type='submit' fluid />
-                    </Form>
-                )}
+            {({ handleSubmit, isSubmitting, errors }) => (
+                <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
+                    <MyTextInput name='email' placeholder='Email' />
+                    <MyTextInput name='password' placeholder='Password' type='password' />
+                    <ErrorMessage
+                        name='error' render={() =>
+                            <Label style={{ marginBottom: 10 }} basic color='red' content={errors.error} />}
+                    />
+                    <Button loading={isSubmitting} positive content='Login' type='submit' fluid />
+                </Form>
+            )}
         </Formik>
     )
 });
